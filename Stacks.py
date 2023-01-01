d57f9eed3266c2afd6b60eec057f2b90ab4da029
Areeb Bin Azim <azim4204261@cloud.neduet.edu.pk>
2023-01-01 13:02:03 +0500

    stacks file added

---
+++ Stacks.py
@@ -0,0 +1,31 @@
+class Stack:
+  def __init__(self): # creates an empty stack
+    self.items = []
+
+  def push(self, item): # adds an item to the top of the stack
+    self.items.append(item)
+
+  def pop(self): # removes and returns the top item
+    return self.items.pop()
+
+  def peek(self): # returns the top item
+    return self.items[-1]
+
+  def is_empty(self): # returns True if the stack is empty
+    return not self.items
+
+
+
+
+
+
+# Test the stack
+stack = Stack() # creates an empty stack
+# print(stack.is_empty())  # prints True (if the stack is empty)
+stack.push(1) # adds 1 to the top of the stack
+stack.push(2) # adds 2 to the top of the stack
+stack.push(3) # adds 3 to the top of the stack
+stack.push(5) # adds 5 to the top of the stack
+print(stack.peek())    # prints 5
+print(stack.pop())   # prints 5 and removes it from the stack
+print(stack.is_empty()) # prints False (if the stack is not empty)
